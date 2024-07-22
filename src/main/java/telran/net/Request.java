7d5df8fadc4e9bfbdd9c2ec07f515e6b321f326b
Yuragranovsky <yuragranovsky@gmail.com>
2024-07-22 21:20:53 +0300

    CW #39 TCP Protocol initial code

---
+++ src/main/java/telran/net/Request.java
@@ -0,0 +1,16 @@
+package telran.net;
+
+import org.json.JSONObject;
+import static telran.net.TcpConfigurationProperties.*;
+public record Request(String requestType, String requestData) {
+
+	@Override
+	public String toString() {
+		JSONObject jsonObj = new JSONObject();
+		jsonObj.put(REQUEST_TYPE_FIELD, requestType);
+		jsonObj.put(REQUEST_DATA_FIELD, requestData);
+		return jsonObj.toString();
+		
+	}
+
+}
